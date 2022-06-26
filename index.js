eaaa7416d8111e602ff9cc6b77a599e3843540be
Tushar Sharma <sharma.tushar9b@gmail.com>
2022-06-27 00:10:00 +0530

    file structure done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//setting up layouts
+const db=require('./config/mongoose');
 const expressLayouts=require('express-ejs-layouts');
-app.use(express.static('./assets'));
-app.use(expressLayouts);
+ //what will happen is that now before rendering any view , server will look for layout.ejs in views and render everything accordingly       
+app.use(expressLayouts);//telling express to use express layouts
+
+app.use(express.static('./assets'));//this is to tell express to look for assets for any css/js/image files
 //extract style and scripts from sub pages into the layouts
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
